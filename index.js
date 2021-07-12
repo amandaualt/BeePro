387047dbb84d9b32579eaf4efd02500312e63c08
Amanda Nobre <nobre@alunos.utfpr.edu.br>
2021-07-12 17:19:00 -0300

    arrumando modal responsivo

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
 var buildUrl = "Build";
-var loaderUrl = buildUrl + "/0.5.loader.js";
-var config = {
-  dataUrl: buildUrl + "/0.5.data.unityweb",
-  frameworkUrl: buildUrl + "/0.5.framework.js.unityweb",
-  codeUrl: buildUrl + "/0.5.wasm.unityweb",
-  streamingAssetsUrl: "StreamingAssets",
-  companyName: "UTFPR",
-  productName: "BeeProv",
-  productVersion: "0.5",
-};
+      var loaderUrl = buildUrl + "/1.loader.js";
+      var config = {
+        dataUrl: buildUrl + "/1.data.unityweb",
+        frameworkUrl: buildUrl + "/1.framework.js.unityweb",
+        codeUrl: buildUrl + "/1.wasm.unityweb",
+        streamingAssetsUrl: "StreamingAssets",
+        companyName: "UTFPR",
+        productName: "BeeProv",
+        productVersion: "1",
+      };
 
-var container = document.querySelector("#unity-container");
-var canvas = document.querySelector("#unity-canvas");
-var loadingBar = document.querySelector("#unity-loading-bar");
-var progressBarFull = document.querySelector("#unity-progress-bar-full");
-var fullscreenButton = document.querySelector("#unity-fullscreen-button");
-var mobileWarning = document.querySelector("#unity-mobile-warning");
+      var container = document.querySelector("#unity-container");
+      var canvas = document.querySelector("#unity-canvas");
+      var loadingBar = document.querySelector("#unity-loading-bar");
+      var progressBarFull = document.querySelector("#unity-progress-bar-full");
+      var fullscreenButton = document.querySelector("#unity-fullscreen-button");
+      var mobileWarning = document.querySelector("#unity-mobile-warning");
 
-// By default Unity keeps WebGL canvas render target size matched with
-// the DOM size of the canvas element (scaled by window.devicePixelRatio)
-// Set this to false if you want to decouple this synchronization from
-// happening inside the engine, and you would instead like to size up
-// the canvas DOM size and WebGL render target sizes yourself.
-// config.matchWebGLToCanvasSize = false;
+      // By default Unity keeps WebGL canvas render target size matched with
+      // the DOM size of the canvas element (scaled by window.devicePixelRatio)
+      // Set this to false if you want to decouple this synchronization from
+      // happening inside the engine, and you would instead like to size up
+      // the canvas DOM size and WebGL render target sizes yourself.
+      // config.matchWebGLToCanvasSize = false;
 
-if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-  container.className = "unity-mobile";
-  // Avoid draining fillrate performance on mobile devices,
-  // and default/override low DPI mode on mobile browsers.
-  config.devicePixelRatio = 1;
-  mobileWarning.style.display = "block";
-  setTimeout(() => {
-    mobileWarning.style.display = "none";
-  }, 5000);
-} else {
-  canvas.style.width = "960px";
-  canvas.style.height = "600px";
-}
-loadingBar.style.display = "block";
+      if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+        container.className = "unity-mobile";
+        // Avoid draining fillrate performance on mobile devices,
+        // and default/override low DPI mode on mobile browsers.
+        config.devicePixelRatio = 1;
+        mobileWarning.style.display = "block";
+        setTimeout(() => {
+          mobileWarning.style.display = "none";
+        }, 5000);
+      } else {
+        canvas.style.width = "960px";
+        canvas.style.height = "600px";
+      }
+      loadingBar.style.display = "block";
 
-var script = document.createElement("script");
-script.src = loaderUrl;
-script.onload = () => {
-  createUnityInstance(canvas, config, (progress) => {
-    progressBarFull.style.width = 100 * progress + "%";
-  }).then((unityInstance) => {
-    loadingBar.style.display = "none";
-    fullscreenButton.onclick = () => {
-      unityInstance.SetFullscreen(1);
-    };
-  }).catch((message) => {
-    alert(message);
-  });
-};
-document.body.appendChild(script);
+      var script = document.createElement("script");
+      script.src = loaderUrl;
+      script.onload = () => {
+        createUnityInstance(canvas, config, (progress) => {
+          progressBarFull.style.width = 100 * progress + "%";
+        }).then((unityInstance) => {
+          loadingBar.style.display = "none";
+          fullscreenButton.onclick = () => {
+            unityInstance.SetFullscreen(1);
+          };
+        }).catch((message) => {
+          alert(message);
+        });
+      };
+      document.body.appendChild(script);
